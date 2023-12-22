830d37ff12004467848c1893e16a4262c495a69a
Kaleab Bayih <kaleabbyh@gmail.com>
2023-12-22 10:37:36 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
+package main
 
+import (
+	"github.com/kaleabbyh/golang-santim-echo/routes"
+	"github.com/labstack/echo/v4"
+)
+
+
+
+func main() {
+
+	router := echo.New()
+	
+	routes.SetupUserRoutes(router)
+	routes.SetupAccountRoutes(router)
+	routes.SetupPaymentRoutes(router)
+	
+	router.Start(":8080")
+	
+}
